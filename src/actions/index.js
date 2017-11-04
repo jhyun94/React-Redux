34bebed22b482de6de0a9e08c01a3ff65b9d2dc2
jhyun94 <hyunjonathan@gmail.com>
2017-11-04 14:05:51 -0500

    created new actioncreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '8ffa32e8fde0194fa77a68dded81e683'
+const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http:openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export function getWeather(city){
+
+  request = axios(`${ROOT_URL}&q=${city},us`);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
+
