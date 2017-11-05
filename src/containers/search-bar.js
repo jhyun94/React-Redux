139a296b39b43559b0e227227bd892ccbd956963
jhyun94 <hyunjonathan@gmail.com>
2017-11-05 11:36:23 -0600

    able to display the city name

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -24,6 +24,7 @@ class SearchBar extends Component{
   onFormSubmit(event){
     event.preventDefault();
     this.props.getWeather( this.state.term);
+    this.setState({term: ''});
   }
 
   render(){
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -4,6 +4,15 @@ import { connect} from 'react-redux';
 
 class WeatherList extends Component{
 
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    )
+  }
+
   render(){
     return(
       <table className="table">
@@ -16,6 +25,7 @@ class WeatherList extends Component{
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     )
