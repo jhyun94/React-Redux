293460b083b2b10aa13ab7f46e44fd8d12109387
jhyun94 <hyunjonathan@gmail.com>
2017-11-05 19:21:18 -0600

    restart

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,2 @@
-import axios from 'axios';
 
-const API_KEY = '54910d727745b59a701ab94e1b89230d'
-export const FETCH_WEATHER = 'FETCH_WEATHER';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-
-
-export function getWeather(city){
-
-  const url = `${ROOT_URL}&q=${city},us`;
-  const request =  axios.get(url);
-
-
-  return {
-    type: FETCH_WEATHER,
-    payload: request
-  }
-}
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import Weather from './reducer_weather';
+
 
 const rootReducer = combineReducers({
   weather: Weather
